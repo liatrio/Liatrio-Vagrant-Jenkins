60c75d39de818ed35c4abc3bbc9cbb47aa0dfd87
Victor Pudeyev <piousbox@Victors-MacBook-Pro.local>
2015-12-02 12:15:11 -0800

    the job now is petclinic-simple-auto-1

--- cookbooks/jenkins/recipes/create_job.rb
+++ cookbooks/jenkins/recipes/create_job.rb
@@ -1,14 +1,17 @@
 
+job_name = node[:jenkins][:job_name]
+job_name = "petclinic-simple-auto-1"
 template '/opt/config.xml' do
-  source   'var/lib/jenkins/jobs/petclinic/config.xml.erb'
+  source   'var/lib/jenkins/jobs/petclinic-simple/config.xml.erb'
   mode     '0755'
   variables({
     :nexus_repo => node[:jenkins][:nexus_repo]
   })
 end
-execute "sleep #{node[:jenkins][:sleep_interval]} && java -jar jenkins-cli.jar -s http://#{node[:jenkins][:ip]} create-job #{node[:jenkins][:job_name]} < config.xml" do
+execute "create_job" do
+  command "java -jar jenkins-cli.jar -s http://#{node[:jenkins][:ip]} create-job #{job_name} < config.xml"
   cwd "/opt"
-  not_if "cd /opt && java -jar jenkins-cli.jar -s http://#{node[:jenkins][:ip]} list-jobs | grep #{node[:jenkins][:job_name]}"
+  not_if "cd /opt && java -jar jenkins-cli.jar -s http://#{node[:jenkins][:ip]} list-jobs | grep #{job_name}"
 end
 
 
